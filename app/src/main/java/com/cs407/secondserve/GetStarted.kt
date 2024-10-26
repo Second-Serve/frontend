df12267c44d2f565cba8fcc5c9cfeb427b7b7dfb
aditisunil <aditisunil13@gmail.com>
2024-10-26 18:36:26 -0500

    page 1 & 2

--- app/src/main/java/com/cs407/secondserve/GetStarted.kt
+++ app/src/main/java/com/cs407/secondserve/GetStarted.kt
@@ -1,4 +1,13 @@
 package com.cs407.secondserve
 
-class GetStarted {
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GetStarted : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.get_started)
+    }
 }
