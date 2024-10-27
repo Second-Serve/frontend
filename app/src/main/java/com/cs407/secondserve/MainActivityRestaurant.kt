a22efd7f7245cdead948bf70b4f1dbcecd755dc0
diyasatanur1 <satanur@wisc.edu>
2024-10-27 18:00:48 -0500

    adding all the files

---
+++ app/src/main/java/com/cs407/secondserve/MainActivityRestaurant.kt
@@ -0,0 +1,33 @@
+package com.cs407.secondserve
+
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+
+class MainActivityRestaurant : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signup_restaurant)
+
+
+        val rootView = findViewById<View>(R.id.main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            view.updatePadding(
+                left = systemBars.left,
+                top = systemBars.top,
+                right = systemBars.right,
+                bottom = systemBars.bottom
+            )
+            insets
+        }
+    }
+}
--- app/src/main/java/com/cs407/secondserve/SignUpUser.kt
+++ app/src/main/java/com/cs407/secondserve/SignUpUser.kt
@@ -34,7 +34,7 @@ class SignUpRestaurant : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_restaurant, container, false)
+        return inflater.inflate(R.layout.fragment_sign_up_user, container, false)
     }
 
     companion object {
