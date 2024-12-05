fb2fb1ecb1c548296b1cda2ad3b9d777f433b251
arushidodhia <arushidodhia@gmail.com>
2024-12-05 15:16:18 -0600

    main page buttons working

--- app/src/main/java/com/cs407/secondserve/RestaurantMainView.kt
+++ app/src/main/java/com/cs407/secondserve/RestaurantMainView.kt
@@ -3,18 +3,51 @@ package com.cs407.secondserve
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class RestaurantMainView : AppCompatActivity() {
+    private var quantity = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_restaurant_sign_up)
+        setContentView(R.layout.restaurant_main_page)
 
-        val signUpButton = findViewById<Button>(R.id.restaurant_sign_up_button)
+        val decreaseButton = findViewById<Button>(R.id.bag_decrease_button)
+        val increaseButton = findViewById<Button>(R.id.bag_increase_button)
+        val quantityText = findViewById<TextView>(R.id.bag_count)
+        val bagPriceInput = findViewById<TextInputEditText>(R.id.bag_price_input)
+        val updateBagPriceButton = findViewById<MaterialButton>(R.id.update_bag_price_button)
 
-        signUpButton.setOnClickListener {
-            setContentView(R.layout.restaurant_main_page)
+        updateBagPriceButton.setOnClickListener {
+            val bagPriceText = bagPriceInput.text.toString()
+            if (bagPriceText.isNotEmpty()) {
+                val bagPrice = bagPriceText.toFloatOrNull() 
+                if (bagPrice != null) {
+                    //save the price here
+                } else {
+                    Toast.makeText(this, "Invalid price", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Please enter a price", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        decreaseButton.setOnClickListener {
+            if (quantity > 0) {
+                quantity--
+                quantityText.text = quantity.toString()
+            }
+        }
+
+        increaseButton.setOnClickListener {
+            quantity++
+            quantityText.text = quantity.toString()
         }
     }
+
+
 }
