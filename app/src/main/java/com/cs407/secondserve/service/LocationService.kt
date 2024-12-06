768cb54e48db93ec39131379d4572c2399b709be
Owen Z <16374972+red40forever@users.noreply.github.com>
2024-12-05 20:19:02 -0600

    Location service stubs

--- app/src/main/java/com/cs407/secondserve/service/LocationService.kt
+++ app/src/main/java/com/cs407/secondserve/service/LocationService.kt
@@ -1,6 +1,16 @@
 package com.cs407.secondserve.service
 
+import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.functions.ktx.functions
+
 class LocationService {
     companion object {
+        fun validateAddres(address: String): Boolean {
+            return true;
+        }
+
+        fun distanceBetween(p1: GeoPoint, p2: GeoPoint): Double {
+            return 0.0;
+        }
     }
 }
